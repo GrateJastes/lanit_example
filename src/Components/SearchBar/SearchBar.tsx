6a5382092c626b35a9e73e7163f049463c96a43c
Nikita <ermilov1999@gmail.com>
2022-04-28 14:33:49 +0300

    tmp

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -3,19 +3,25 @@ import Select from 'react-select';
 import './SearchBar.scss'
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import consts from '../../consts';
-import {clear} from '../../Feautures/suggester/suggesterSlice';
+import { clear } from '../../Feautures/suggester/suggesterSlice';
 import { fetchSuggestions } from '../../Feautures/suggester/suggesterSlice';
 // @ts-ignore
 import ymaps from 'ymaps';
 
 function SearchBar() {
-    const suggestionsList = useAppSelector((state) => state.suggestions);
+    const suggestionsList = useAppSelector((state) => state.suggestions)
+        .map((suggestion) => ({
+            // @ts-ignore
+            value: suggestion.value,
+            // @ts-ignore
+            label: suggestion.displayName,
+        }));
     const dispatch = useAppDispatch();
 
     const refreshSuggestions = (searchQuery: string) => {
-        if (searchQuery.length <= consts.minSearchLength) {
-            dispatch(clear())
-        }
+        // if (searchQuery.length <= consts.minSearchLength) {
+        //     dispatch(clear())
+        // }
         if (searchQuery.length >= consts.minSearchLength) {
             dispatch(fetchSuggestions(searchQuery));
         }
